4c920fbc795ec01ee85bb12c0198f78d013d6c3f
Alexandre Strube <surak@surak.eti.br>
2016-03-15 00:53:26 +0100

    Calendar view, fixed save uptime Cleaned up db

--- saveuptime.sh
+++ saveuptime.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "insert into solaruptime values(datetime('now'),\"$(cat /proc/uptime | cut -f1 -d.)\");" | sqlite3 ~/Devel/Solar/uptime.db
+echo "insert into solaruptime values(strftime('%Y-%m-%d','now'),strftime('%H:%M','now'),\"$(cat /proc/uptime | cut -f1 -d.)\");" | sqlite3 ~/Devel/Solar/uptime.db
