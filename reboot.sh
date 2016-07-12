197acff0e462fb1b30346f6dc7a72dc6f0a53343
Alexandre Strube (pi2) <surak@surak.eti.br>
2016-07-13 01:04:08 +0200

    reboot script

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ACCESS_KEY=$(cat $HOME/.ssh/initialstate-accesskey)
 KEY=$(cat $HOME/.ssh/initialstate-uptime-bucket)
-
-curl 'https://groker.initialstate.com/api/events?accessKey='$ACCESS_KEY'&bucketKey='$KEY'&Event=Boot'
-
+REBOOT_DATE=$(date +%Y-%m-%d)
+REBOOT_HOUR=$(date +%H:%M)
+( sleep 30; curl 'https://groker.initialstate.com/api/events?accessKey='$ACCESS_KEY'&bucketKey='$KEY'&Event=Last%20Reboot:'$REBOOT_DATE'%20'$REBOOT_HOUR ) &
