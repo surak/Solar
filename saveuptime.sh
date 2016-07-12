45ae4646c0909f839198b7a4197030fc56411035
Alexandre Strube (pi2) <surak@surak.eti.br>
2016-07-13 00:03:25 +0200

    The saveuptime adds the uptime also to initialstate.com (I wish I knew this before).

--- saveuptime.sh
+++ saveuptime.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "insert into solaruptime values(strftime('%Y-%m-%d','now'),strftime('%H:%M','now'),\"$(cat /proc/uptime | cut -f1 -d.)\");" | sqlite3 ~/Devel/Solar/uptime.db
+UPTIME=$(cat /proc/uptime | cut -f1 -d.)
+ACCESS_KEY=$(cat $HOME/.ssh/initialstate-accesskey)
+KEY=$(cat $HOME/.ssh/initialstate-uptime-bucket)
+
+echo "insert into solaruptime values(strftime('%Y-%m-%d','now'),strftime('%H:%M','now'),\"$UPTIME\");" | sqlite3 ~/Devel/Solar/uptime.db
+curl 'https://groker.initialstate.com/api/events?accessKey=S1yBbnK5usBUo2TXErEmIs89bfBq62Bj&bucketKey='$KEY'&Uptime='$UPTIME
+
