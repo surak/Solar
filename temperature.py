d606b5e752b1fa5b9571a868e07ab5d464da71aa
Alexandre Strube <surak@surak.eti.br>
2017-02-22 00:15:15 +0100

    temp

--- temperature.py
+++ temperature.py
@@ -2,9 +2,9 @@
 import sys
 import Adafruit_DHT
 
-for i in range(1,10):
-#while True:
+#for i in range(1,10):
 
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+while True:
+      humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+      print 'Temp: ', temperature, 'humidity: ',humidity
+      #print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
